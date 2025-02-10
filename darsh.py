84833fc929ca457d1f15d429635d2c1541448e7c
Darshnikanagle <darshnikanagle38@gmail.com>
2025-02-10 17:37:23 +0530

    Add files via upload

---
+++ darsh.py
@@ -0,0 +1,27 @@
+print("Hello Darsh")
+print("Hello World") # This line prints Hello World
+
+
+
+
+# # Simple program to add two numbers
+# num1 = 5
+# num2 = 3
+# sum = num1 + num2
+# print("The sum is:", sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
